ead52cc12b90b37080e76bb85a02b4ae93afd3a2
RK Xu <xvrongkang@outlook.com>
2025-04-24 15:23:17 +0800

    feat: add Toc Sidebar

--- src/components/editor/plate-editor.tsx
+++ src/components/editor/plate-editor.tsx
@@ -6,9 +6,10 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import { Plate } from '@udecode/plate/react';
 
-import { useCreateEditor } from '@/components/editor/use-create-editor';
 import { SettingsDialog } from '@/components/editor/settings';
+import { useCreateEditor } from '@/components/editor/use-create-editor';
 import { Editor, EditorContainer } from '@/components/plate-ui/editor';
+import {TocSidebar} from '@/components/plate-ui/toc-sidebar';
 
 export function PlateEditor() {
   const editor = useCreateEditor();
@@ -16,6 +17,7 @@ export function PlateEditor() {
   return (
     <DndProvider backend={HTML5Backend}>
       <Plate editor={editor}>
+        <TocSidebar className="top-[130px]" topOffset={30} />
         <EditorContainer>
           <Editor variant="demo" />
         </EditorContainer>
